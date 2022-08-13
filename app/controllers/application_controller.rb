c79220cb69492a3627413bbf4d9ef5aaa0ce48c4
Jaime Montes de Oca <jaime0422@live.com>
2022-08-13 11:28:34 -0700

    Services end points done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,44 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
     
-    get "/" do
-        { message: "Good luck with your project!" }.to_json
+    # Services end points
+    # ===================
+    get "/services" do
+        Service.all.to_json
+    end
+
+    post "/services" do
+        service = Service.create(
+            description: params[:description]
+        )
+
+        service.to_json
+    end
+
+    patch "/services/:id" do
+        service = Service.find(params[:id])
+        service.update(description: params[:description])
+
+        service.to_json
+    end
+
+    delete "/services/:id" do
+        service = Service.find(params[:id])
+        service.destroy
+
+        service.to_json
+    end
+
+    # Locations end points
+    # ====================
+    get "/locations" do
+        Location.all.to_json
+    end
+
+    # Vehicles end points
+    # ===================
+    get "/vehicles" do
+        Vehicle.all.to_json
     end
 
 end
