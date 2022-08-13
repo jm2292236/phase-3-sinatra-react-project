db317087ee1444a04348eabcbb879dc17b0333d0
Jaime Montes de Oca <jaime0422@live.com>
2022-08-13 11:34:40 -0700

    Locations end points done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,28 @@ class ApplicationController < Sinatra::Base
         Location.all.to_json
     end
 
+    post "/locations" do
+        location = Location.create(
+            name: params[:name]
+        )
+
+        location.to_json
+    end
+
+    patch "/locations/:id" do
+        location = Location.find(params[:id])
+        location.update(name: params[:name])
+
+        location.to_json
+    end
+
+    delete "/locations/:id" do
+        location = Location.find(params[:id])
+        location.destroy
+
+        location.to_json
+    end
+
     # Vehicles end points
     # ===================
     get "/vehicles" do
